f2c5eef32e690eea4232fe5f821095bbf8658220
Gonzalo DCL <gonzalo@decentraland.org>
2025-05-15 18:54:47 -0300

    remove unnecessary files

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
-import { engine, Entity, InputAction, Material, pointerEventsSystem, RealmInfo, Schemas } from '@dcl/sdk/ecs'
+import { engine, Entity, InputAction, Material, pointerEventsSystem, RealmInfo, Schemas, SyncComponents, Transform } from '@dcl/sdk/ecs'
 import { createCube } from './factory'
-import { changeColorSystem } from './systems'
-
 import { syncEntity } from '@dcl/sdk/network'
-import { setupUi } from './ui'
 
-export const Click = engine.defineComponent('click', { count: Schemas.Number })
-export let clickEntity: Entity
-
-(globalThis as any).DEBUG_NETWORK_MESSAGES = true
 
 export async function main() {
-  const userCube = createCube(Math.random() * 8, 1, Math.random() * 8)
-  syncEntity(userCube, [Click.componentId, Material.componentId])
-  changeColorSystem()
+  // setupUi()
+  // syncEntity(userCube, [Click.componentId, Material.componentId])
+
   // setupUi()
   if (false) {
     // console.log('__DEV__', __DEV__)
@@ -46,3 +39,58 @@ export async function main() {
 
   // setupUi()
 }
+
+export const Click = engine.defineComponent('click', { count: Schemas.Number })
+export let clickEntity: Entity
+
+
+
+= engine.addEntity()
+      // // Add door capabilities.
+
+      // // open/close the door on click
+      // pointerEventsSystem.onPointerDown(doorEntity, () => {
+      //   const doorState = DoorState.getMutable(doorEntity)
+      //   doorState.open = !doorState.open
+      // })
+
+      // if (isServer()) {
+      //   // Validate the received message
+      //   DoorState.onChangeValidation(doorEntity, (newValue, currentValue, address) => {
+      //     if (address === serverAddress) return true
+      //     if (!ALLOWED_LIST.includes(address)) return false
+
+      //     if (hasEnoughMana(address) && hasCoinInHand(address)) {
+      //       return true
+      //     }
+
+      //     return false
+      //   })
+
+      //   // Apply some logic when the door opens/close.
+      //   DoorState.onChange(doorEntity, () => {
+      //     Transform.getMutable(doorEntity).rotation = Vector3.create(12, 1, 12)
+      //     Material.getMutable(doorEntity).material
+      //   })
+      // }
+// function isServer() {
+//   return true
+// }
+
+// const GameState = Schemas.Array(Schemas.Map({
+//   address: Schemas.String,
+//   color: Schemas.Color4,
+//   score: Schemas.Number
+// }))
+
+
+//   function hasEnoughMana(address: string) {
+//     myProfile.userId
+//     return true
+//   }
+
+
+
+//       // const doorEntity
+
+
